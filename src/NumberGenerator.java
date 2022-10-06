9eb33b8f9c1b3cbfe14c5a1b763da36526a14fa0
dokim <dogdo5726@gmail.com>
2022-10-06 11:39:20 +0900

    readme add

--- src/NumberGenerator.java
+++
@@ -1,3 +0,0 @@
-public interface NumberGenerator {
-    char generate(int num);
-}
--- src/RandomNumberGenerator.java
+++
@@ -1,9 +0,0 @@
-import W3_D3_221006.HashSet_Alpa.NumberGenerator;
-
-public class RandomNumberGenerator implements NumberGenerator {
-
-    @Override
-    public char generate(int num) {
-        return (char)(Math.random() * num + 65); // A~Z 생성
-    }
-}
