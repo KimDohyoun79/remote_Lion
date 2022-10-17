e8aa68a130c25b7ed67c9189d14704f88fc15f8e
dokim <dogdo5726@gmail.com>
2022-10-17 09:22:24 +0900

    Star!!

--- src/Star01.java
+++ src/Star01.java
@@ -1,11 +1,18 @@
 public class Star01 {
 
+    public static void printStar(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
-        System.out.println("*****");
+        int n = 5;
+        printStar(n);
+
     }
 }
