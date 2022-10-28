aa75e192c271e06375b819820cc627c36f9fd61b
dokim <dogdo5726@gmail.com>
2022-10-28 09:40:00 +0900

    Programmers_Hash

---
+++ src/Programmers_Hash01.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+
+public class Programmers_Hash01 {
+    public static void main(String[] args) {
+        HashSolution hash = new HashSolution();
+        int[][] test = {{3, 1, 2, 3}, {3, 3, 3, 2, 2, 4}, {3, 3, 3, 2, 2, 2}};
+
+        System.out.println(hash.solution(test[2]));
+    }
+}
+
+/*
+    1. 반복문으로 HeshSet에 n을 하나씩 add한다.
+    2. n.length/2 와 hesh.size 비교
+    3. n.length/2 < hesh.size 이면  n.length/2 return
+    4. 아니면 hesh.size
+ */
+
+class HashSolution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        HashSet set = new HashSet();
+
+        for (int n : nums) {
+            set.add(n);
+        }
+
+        answer = nums.length / 2 < set.size() ? nums.length / 2 : set.size();
+
+        return answer;
+    }
+}
