c7d86308153a6ca4358f7d53ea065c5b38757ae1
dokim <dogdo5726@gmail.com>
2022-10-31 11:01:58 +0900

    전화번호 목록 RESTART

--- src/Progremmers_prepareTest.java
+++ src/Progremmers_prepareTest.java
@@ -1,35 +1,21 @@
 import java.util.Arrays;
 
 public class Progremmers_prepareTest {
-
     public static int[] solution(int[] answers) {
         int[] result = new int[3];
 
         int[][] test = {
-                {1, 2, 3, 4, 5, 1, 2, 3, 4, 5},
-                {2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5},
-                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+                {1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
 
-
-        for (int i = 0; i < 3; i++) {
-            int cnt = 0;
-            for (int j = 0; j < test[i].length; j++) {
-                if (test[i][j] == answers[j % 5]) {
-                    cnt++;
-                }
-            }
-            if (cnt > 0) {
-                if (cnt >= result[i])
-                    result[i] = i + 1;
-            }
-        }
 
         return result;
     }
 
     public static void main(String[] args) {
-        int[] answers = {1, 2, 3, 4, 5};
-        int[] result = solution(answers);
+        int[][] answers = {{1, 2, 3, 4, 5}, {1, 3, 2, 4, 2}};
+        int[] result = solution(answers[0]);
 
         System.out.println(Arrays.toString(result));
     }
