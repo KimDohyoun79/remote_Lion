8b72142b594f14b3b033719695b4faa2911b2fc9
dokim <dogdo5726@gmail.com>
2022-10-31 11:32:54 +0900

    전화번호 목록 Clear

--- src/Progremmers_prepareTest.java
+++ src/Progremmers_prepareTest.java
@@ -1,21 +1,48 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Progremmers_prepareTest {
     public static int[] solution(int[] answers) {
-        int[] result = new int[3];
+        int[] cnt = {0, 0, 0};
 
-        int[][] test = {
-                {1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5},
-                {2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5},
-                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+        int[] one = {1, 2, 3, 4, 5};
+        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == one[i % 5])
+                cnt[0]++;
+            if (answers[i] == two[i % 8])
+                cnt[1]++;
+            if (answers[i] == three[i % 10])
+                cnt[2]++;
+        }
 
-        return result;
+        int max = 0;
+        for (int cntNum: cnt) {
+            if(cntNum >= max){
+                max = cntNum;
+            }
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            if(max == cnt[i]){
+                list.add(i+1);
+            }
+        }
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
         int[][] answers = {{1, 2, 3, 4, 5}, {1, 3, 2, 4, 2}};
-        int[] result = solution(answers[0]);
+        int[] result = solution(answers[1]);
 
         System.out.println(Arrays.toString(result));
     }
