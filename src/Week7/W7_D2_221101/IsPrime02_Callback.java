0d5a57b73728b447fea6f963b25395e187342b77
dokim <dogdo5726@gmail.com>
2022-11-01 10:45:28 +0900

    java callback

--- src/Week7/W7_D2_221101/IsPrime02_Callback.java
+++ src/Week7/W7_D2_221101/IsPrime02_Callback.java
@@ -1,14 +1,19 @@
 package Week7.W7_D2_221101;
 
+interface StatemaentStrategy {
+    boolean compare(int a, int b);
+}
+
+
 public class IsPrime02_Callback {
 
-    static boolean someOperation(int a, int b){
-        return a< b;
+    static boolean someOperation(int a, int b) {
+        return a < b;
     }
 
-    static boolean isPrime01(int num) {
+    static boolean isPrime01(int num, StatemaentStrategy stmt) {
 
-        for (int i = 2; someOperation(i, num); i++) {
+        for (int i = 2; stmt.compare(i, num); i++) {
             if (num % 2 == 0)
                 return false; // 나누어 지면 소수가 아니다.
         }
@@ -18,10 +23,11 @@ public class IsPrime02_Callback {
 
 
     public static void main(String[] args) {
-
+        IsPrime02_Callback cb = new IsPrime02_Callback();
         int num = 12;
-        System.out.println("isPrime01 : " + isPrime01(num) + "\n");
-        //System.out.println("isPrime02 : " + isPrime02(num) + "\n");
-        //System.out.println("isPrime03 : " + isPrime03(num) + "\n");
+
+        System.out.println(cb.isPrime01(num, ((a, b) -> a < b)));
+        System.out.println(cb.isPrime01(num, ((a, b) -> a < b / 2)));
+        System.out.println(cb.isPrime01(num, ((a, b) -> a * a < b)));
     }
 }
