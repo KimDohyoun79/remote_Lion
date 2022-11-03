79b0922e23b963861324bd951f051d21cb5bf0fd
dokim <dogdo5726@gmail.com>
2022-11-03 09:21:30 +0900

    에라토스테네스의 체 프로그래머스 소수찾기 시간

--- src/Week7/IsPrimeEratos.java
+++ src/Week7/IsPrimeEratos.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class IsPrimeEratos {
 
     static int isPrimeEratos(int num) {
-        //long start = System.nanoTime();
+        long start = System.nanoTime();
 
         boolean[] numArr = new boolean[num+1];// 0 ~ num 까지 담을 배열
 
@@ -25,16 +25,70 @@ public class IsPrimeEratos {
                 cnt++;
         }
 
-        //long end = System.nanoTime();
-        //System.out.println("isPrimeEratos 실행시간 : " + (end - start) / 10000000.0);
+        long end = System.nanoTime();
+        System.out.println("isPrimeEratos 실행시간 : " + (end - start) / 1000000000.0);
 
         return cnt;
     }
 
 
+
+    // 방법 1 : 2 ~ 자기보다 작은 수까지(num-1까지) 하나씩 나누어 소수인지 확인
+    static boolean isPrime01(int num) {
+        long start = System.nanoTime();
+
+        for (int i = 2; i < num; i++) {
+            if (num % 2 == 0)
+                return false; // 나누어 지면 소수가 아니다.
+        }
+
+        long end = System.nanoTime();
+        System.out.println("isPrime01 실행시간 : " + (end - start) / 1000000000.0);
+
+        return true; // 위 조건을 통과 했으면 소수다.
+    }
+
+
+    // 방법 2 : (2 ~ num/2) 까지 하나씩 나누어 소수인지 확인 => num/2보다 큰 수로는 소수점으로 나누어 진다.
+    static boolean isPrime02(int num) {
+        long start = System.nanoTime();
+
+        for (int i = 2; i < num/2; i++) {
+            if (num % 2 == 0)
+                return false;
+        }
+
+        long end = System.nanoTime();
+        System.out.println("isPrime02 실행시간 : " + (end - start) / 1000000000.0);
+
+        return true;
+    }
+
+
+    // 방법 3 : (2 ~ num 제곱근) 까지 하나씩 나누어 소수인지 확인 => num/2보다 큰 수로는 소수점으로 나누어 진다.
+    // i * i < num  같은 결과 i< Math.sqrt(num)
+    // Math.sqrt() 연산이 많다. 그래서 i * i < num 방식이 더 좋을 수도 있다.
+    static boolean isPrime03(int num) {
+        long start = System.nanoTime();
+
+        for (int i = 2; i < Math.sqrt(num); i++) {
+            if (num % 2 == 0)
+                return false;
+        }
+
+        long end = System.nanoTime();
+        System.out.println("isPrime03 실행시간 : " + (end - start) / 1000000000.0);
+
+        return true;
+    }
+
+
     public static void main(String[] args) {
 
-        int num = 5;
-        System.out.println("isPrime01 : " + isPrimeEratos(num) + "\n");
+        int num = 14535333;
+        System.out.println("isPrimeEratos : " + isPrimeEratos(num) + "\n");
+        System.out.println("isPrime01 : " + isPrime01(num) + "\n");
+        System.out.println("isPrime02 : " + isPrime02(num) + "\n");
+        System.out.println("isPrime03 : " + isPrime03(num) + "\n");
     }
 }
