7308d533117fbd66d16b94eb2477b87232d21c0e
dokim <dogdo5726@gmail.com>
2022-11-03 10:52:54 +0900

    에라토스테네스의 체 프로그래머스 소수찾기 시간체크

--- src/Week7/IsPrimeEratos.java
+++ src/Week7/IsPrimeEratos.java
@@ -6,89 +6,63 @@ import java.util.List;
 
 public class IsPrimeEratos {
 
+    public static int solution(int N) {
+        long start = System.nanoTime();
+
+        if (N == 2) return 1;
+        int[] nums = new int[N-1]; // N-1개만큼 2~50까지 총 49개
+        boolean[] checks = new boolean[nums.length]; // nums만큼 checks생성
+        Arrays.fill(checks, true); // checks를 true로 초기화 cuz 기본값 false
+        for (int i = 0; i < nums.length; i++) nums[i] = i + 2; // 2~49까지 nums에 채우기
+
+        for (int j = 0; j * j <= N; j++) {// N == 49
+            int multipleOf = nums[j]; // j=0 nums[0] = 2
+            // 초항이 2 2n인 2 4 6 8 10
+            for (int i = nums[j] + j; i < nums.length; i+=multipleOf) {
+                checks[i] = false;
+            }
+//            printNums(nums, checks);
+        }
+        int cnt = 0;
+        for (int i = 0; i < checks.length; i++) {
+            if(checks[i]) cnt++;
+        }
+        long end = System.nanoTime();
+        System.out.println("isPrimeEratos 실행시간 : " + (end - start) / 1000_000_000.0);
+        return cnt;
+    }
+
+
     static int isPrimeEratos(int num) {
         long start = System.nanoTime();
 
-        boolean[] numArr = new boolean[num+1];// 0 ~ num 까지 담을 배열
-
+        boolean[] numArr = new boolean[num + 1];// 0 ~ num 까지 담을 배열
+        //Arrays.fill(numArr, true);
         numArr[0] = numArr[1] = true;   // 0과 1은 소수가 아니므로 true
         for (int i = 2; i <= Math.sqrt(num); i++) {
-            for (int j = i; j <= num; j+=i) {
+            for (int j = i; j <= num; j += i) {
                 if (j % i == 0 && j / i != 1)
                     numArr[j] = true;   // 소수가 아니면 true
             }
         }
 
-        int cnt =0;
+        int cnt = 0;
         for (int i = 0; i <= num; i++) {
-            if(!numArr[i])
+            if (!numArr[i])
                 cnt++;
         }
 
         long end = System.nanoTime();
-        System.out.println("isPrimeEratos 실행시간 : " + (end - start) / 1000000000.0);
+        System.out.println("isPrimeEratos 실행시간 : " + (end - start) / 1000_000_000.0);
 
         return cnt;
     }
 
 
-
-    // 방법 1 : 2 ~ 자기보다 작은 수까지(num-1까지) 하나씩 나누어 소수인지 확인
-    static boolean isPrime01(int num) {
-        long start = System.nanoTime();
-
-        for (int i = 2; i < num; i++) {
-            if (num % 2 == 0)
-                return false; // 나누어 지면 소수가 아니다.
-        }
-
-        long end = System.nanoTime();
-        System.out.println("isPrime01 실행시간 : " + (end - start) / 1000000000.0);
-
-        return true; // 위 조건을 통과 했으면 소수다.
-    }
-
-
-    // 방법 2 : (2 ~ num/2) 까지 하나씩 나누어 소수인지 확인 => num/2보다 큰 수로는 소수점으로 나누어 진다.
-    static boolean isPrime02(int num) {
-        long start = System.nanoTime();
-
-        for (int i = 2; i < num/2; i++) {
-            if (num % 2 == 0)
-                return false;
-        }
-
-        long end = System.nanoTime();
-        System.out.println("isPrime02 실행시간 : " + (end - start) / 1000000000.0);
-
-        return true;
-    }
-
-
-    // 방법 3 : (2 ~ num 제곱근) 까지 하나씩 나누어 소수인지 확인 => num/2보다 큰 수로는 소수점으로 나누어 진다.
-    // i * i < num  같은 결과 i< Math.sqrt(num)
-    // Math.sqrt() 연산이 많다. 그래서 i * i < num 방식이 더 좋을 수도 있다.
-    static boolean isPrime03(int num) {
-        long start = System.nanoTime();
-
-        for (int i = 2; i < Math.sqrt(num); i++) {
-            if (num % 2 == 0)
-                return false;
-        }
-
-        long end = System.nanoTime();
-        System.out.println("isPrime03 실행시간 : " + (end - start) / 1000000000.0);
-
-        return true;
-    }
-
-
     public static void main(String[] args) {
 
-        int num = 14535333;
+        int num = 24535333;
         System.out.println("isPrimeEratos : " + isPrimeEratos(num) + "\n");
-        System.out.println("isPrime01 : " + isPrime01(num) + "\n");
-        System.out.println("isPrime02 : " + isPrime02(num) + "\n");
-        System.out.println("isPrime03 : " + isPrime03(num) + "\n");
+        System.out.println("solution : " + solution(num) + "\n");
     }
 }
