1195b83f381ad70e08b92bdf85a7a26d2f543109
dokim <dogdo5726@gmail.com>
2022-11-07 09:44:20 +0900

    나누어 떨어지는 숫자 배열 start

--- README.md
+++ README.md
@@ -102,7 +102,7 @@
 
 ### W8_D1_221107
 - `HarshadNumber` : [프로그래머스 하샤드 수](https://school.programmers.co.kr/learn/courses/30/lessons/12947)
-
+- `DivisorArray` : [프로그래머스 나누어 떨어지는 숫자 배열](https://school.programmers.co.kr/learn/courses/30/lessons/12910) 
 
 
 </details>
---
+++ src/DivisorArray.java
@@ -0,0 +1,21 @@
+/*
+하샤드 수
+https://school.programmers.co.kr/learn/courses/30/lessons/12947
+
+ */
+
+public class DivisorArray {
+    public static int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[][]  arr = {{5, 9, 7, 10}, {2, 36, 1, 3}, {3, 2, 6}};
+        System.out.println(solution(arr[0], 5));
+        System.out.println(solution(arr[1], 1));
+        System.out.println(solution(arr[2], 10));
+    }
+}
