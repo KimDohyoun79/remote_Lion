4aacd1f9235de6833269470f1570ea25b23f15d9
dokim <dogdo5726@gmail.com>
2022-11-07 09:16:28 +0900

    하샤드수 clear

--- src/test.java
+++ src/test.java
@@ -7,6 +7,15 @@ https://school.programmers.co.kr/learn/courses/30/lessons/12947
 public class test {
     public static boolean solution(int x) {
         boolean answer = true;
+
+        int sum = 0;
+        for (int i = 1; i <= x; i++) { // 자릿수의 합 구하기
+            sum += x;
+        }
+
+        if(sum%x != 0) // 나누어 떨어지는지 check하기
+            answer = false;
+
         return answer;
     }
 
