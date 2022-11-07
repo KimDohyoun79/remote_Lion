d0155012dd3d34d89b3a8f2d0c8b07139cfe4779
dokim <dogdo5726@gmail.com>
2022-11-07 09:12:36 +0900

    하샤드수 start

---
+++ src/test.java
@@ -0,0 +1,19 @@
+/*
+하샤드 수
+https://school.programmers.co.kr/learn/courses/30/lessons/12947
+
+ */
+
+public class test {
+    public static boolean solution(int x) {
+        boolean answer = true;
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        int num = 21;
+        System.out.println(solution(num));
+    }
+}
