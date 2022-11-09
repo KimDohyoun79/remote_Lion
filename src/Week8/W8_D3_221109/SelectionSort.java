a5da4e5acf017d968aa25bb5289eda68f5e188b9
dokim <dogdo5726@gmail.com>
2022-11-09 09:47:18 +0900

    [add] 선택정렬

--- src/Week8/W8_D3_221109/SelectionSort.java
+++ src/Week8/W8_D3_221109/SelectionSort.java
@@ -1,6 +1,7 @@
 package Week8.W8_D3_221109;
 //선택정렬
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SelectionSort {
@@ -9,13 +10,22 @@ public class SelectionSort {
         int[] arr = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
 
         int min = -1;
-        int minIndex = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i] < min){
-                min=arr[i];
-                minIndex=i;
+        int minIndex;
+        for (int i = 0; i < arr.length -1; i++) {
+            minIndex=i;
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[minIndex] > arr[j]){
+                    minIndex=j;
+                }
             }
+            swap(arr, minIndex, i);
         }
-        System.out.println(minIndex);
+
+        System.out.println(Arrays.toString(arr));
+    }
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
